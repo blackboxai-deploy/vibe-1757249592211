3febcb6152790a7fb1d5e976d2e1a2cb9d9fdcba
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-07 18:23:40 +0530

    Add src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view />
+</template>
+
+<script setup lang="ts">
+// This is the main App component for the Quasar Vue 3 application
+// Using Composition API with script setup syntax
+</script>
+
+<style lang="scss">
+.q-page {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+</style>
